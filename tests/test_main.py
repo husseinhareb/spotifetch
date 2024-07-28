a6530672b1bf8d294e671a670800e8d17a200f26
husseinhareb <shtam.hareb12@gmail.com>
2024-07-28 21:20:20 +0300

    setting up files

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import unittest
+from src.main import main
+
+class TestMain(unittest.TestCase):
+    def test_main(self):
+        # Write your tests here
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
