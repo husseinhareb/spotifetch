0fdf97a87f072b7a71483a860c366fdc01cbf1ba
husseinhareb <shtam.hareb12@gmail.com>
2024-09-08 15:19:15 +0200

    playback of current song

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {
-  useUsername,
-} from '../../services/store';
+import { useUsername } from '../../services/store';
 
 const Home: React.FC = () => {
   const username = useUsername();
@@ -10,6 +8,8 @@ const Home: React.FC = () => {
   const [currentArtist, setCurrentArtist] = useState<string | null>(null);
   const [albumImage, setAlbumImage] = useState<string | null>(null);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [progressMs, setProgressMs] = useState<number | null>(0);
+  const [durationMs, setDurationMs] = useState<number | null>(0);
 
   useEffect(() => {
     const fetchCurrentSong = async () => {
@@ -24,6 +24,8 @@ const Home: React.FC = () => {
             setCurrentArtist(songInfo.artist_name);
             setAlbumImage(songInfo.album_image);
             setIsPlaying(songInfo.is_playing);
+            setProgressMs(songInfo.progress_ms);
+            setDurationMs(songInfo.duration_ms);
           }
         } else {
           console.error("Failed to fetch current song");
@@ -36,16 +38,24 @@ const Home: React.FC = () => {
     fetchCurrentSong();
   }, []);
 
+  // Helper function to format time in mm:ss
+  const formatTime = (ms: number | null) => {
+    if (ms === null) return '0:00';
+    const minutes = Math.floor(ms / 60000);
+    const seconds = Math.floor((ms % 60000) / 1000);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
   return (
     <div>
       <p>Welcome home, {username}!</p>
       {isPlaying ? (
         <div>
-          <h1>{isPlaying}</h1>
           <h2>Currently Playing:</h2>
           <p><strong>Track:</strong> {currentTrack}</p>
           <p><strong>Artist:</strong> {currentArtist}</p>
           {albumImage && <img src={albumImage} alt="Album cover" />}
+          <p><strong>Progress:</strong> {formatTime(progressMs)} / {formatTime(durationMs)}</p>
         </div>
       ) : (
         <p>No song is currently playing.</p>
